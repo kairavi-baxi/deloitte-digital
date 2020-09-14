48a9f6049e341a193d6da0a20f4222e5759bed9b
kairavi-baxi <k4kairavi@gmail.com>
2020-09-14 19:04:58 +0530

    interview

---
+++ product.js
@@ -0,0 +1,40 @@
+var app = angular.module('myApp', []);
+app.controller("myAppController", function($scope){
+    $scope.products = [
+        {
+            product_name: 'Coombes',
+            images:'assets/img/products/coombes.jpg',
+            price:'$2,600',
+            type:'Lounge'
+        },
+        {
+            product_name: 'Keeve set',
+            images:'assets/img/products/keeve.jpg',
+            price:'$590',
+            type:'Table and Chairs'
+        },
+        {
+            product_name: 'Momo',
+            images:'assets/img/products/book.jpg',
+            price:'$890',
+            type:'Shelves'
+        }
+    ]
+
+    $scope.searchFn = function(){
+            var data = $scope.products;
+           var a = _.findWhere(data, {product_name:$scope.searchText});
+            if(a){
+                $scope.products = [];
+                $scope.products.push(a);
+                $scope.nodata = false;
+            }else{
+                $scope.nodata = true;
+            }
+    }
+
+    $scope.resetFn = function(){
+        $scope.searchText = undefined;
+        $scope.nodata = false;
+    }
+});
